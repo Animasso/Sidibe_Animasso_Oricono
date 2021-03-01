34593010dadc06a533839aaffc0be61c41ced558
Animasso <60935027+Animasso@users.noreply.github.com>
2021-03-01 13:19:24 +0100

    ajout

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -26,17 +26,30 @@ function chargeProduit(){
     request.send();
 }
 /*Fonction pour la sélection et l'affichage du produit selectionné dans la page produit */
-let productId=window.location.href.split[1];
-console.log(productId);
+
 
 function selectProduct(){
+    let productId=window.location.href.split["?id="][1];
+    console.log(productId);
     var request=new XMLHttpRequest();
-    request.open('GET','http://localhost:3000/api/teddies'+ productId);
+    request.open('GET','http://localhost:3000/api/teddies/'+ productId);
     request.onreadystatechange=function () {
         if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
             var response = JSON.parse(request.responseText);
             console.log(response);
+                document.getElementById('produit').innerHTML += `
+               <div class="teddie1">
+                  <a href="produit.html?id=${response[i]._id}">
+                     <figure><img src="${response[i].imageUrl}" alt=""></figure>
+                  </a>
+                  <div class="descriptionPrice">
+                   <div class="name">${response[i].name} </div>
+                   <div class="description">${response[i].description} </div>
+                   <div class="prix">${response[i].price / 100} eur</div>
+                  </div>
+                </div>
+              `;
+            }
         }
-}
+    }
 
-}
